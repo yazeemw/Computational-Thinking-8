836905531f23222f12792b1e5dfa4f3c2ec3f852
yazeemw <yazeewilson@seattleacademy.org>
2025-12-11 22:28:00 +0000

    hi

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("I have an older sister")
+print("I play volleyball")
+print("I've never been to disney land")
---
+++ jokes.py
@@ -0,0 +1,3 @@
+print("What do you call a belt made of watches?")
+input("")
+print("A waist of time")
